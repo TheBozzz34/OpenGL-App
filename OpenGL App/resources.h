3c47649bfed98fba76a6309d9e58104f66aa81d3
Ethan James <necrozma@catgirlsaresexy.org>
2025-02-11 21:39:13 -0700

    Initial Commit

---
+++ OpenGL App/resources.h
@@ -0,0 +1,17 @@
+#pragma once
+#ifndef RESOURCES_H
+#define RESOURCES_H
+
+// Antonio_Bold_ttf 
+extern unsigned char Antonio_Bold_ttf[];
+extern unsigned int Antonio_Bold_ttf_len;
+
+// File 2
+extern unsigned char text_vs[];
+extern unsigned int text_vs_len;
+
+// File 3
+extern unsigned char text_fs[];
+extern unsigned int text_fs_len;
+
+#endif // RESOURCES_H
